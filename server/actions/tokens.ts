88f600a91479f052c9137223878d5e79660251b8
developedbyed <ed.magician@gmail.com>
2024-06-13 23:04:59 +0100

    token bug

--- server/actions/tokens.ts
+++ server/actions/tokens.ts
@@ -53,10 +53,14 @@ export const newVerification = async (token: string) => {
     where: eq(users.email, existingToken.email),
   })
   if (!existingUser) return { error: "Email does not exist" }
-  await db.update(users).set({
-    emailVerified: new Date(),
-    email: existingToken.email,
-  })
+
+  await db
+    .update(users)
+    .set({
+      emailVerified: new Date(),
+      email: existingToken.email,
+    })
+    .where(eq(users.id, existingUser.id))
 
   await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id))
   return { success: "Email Verified" }
